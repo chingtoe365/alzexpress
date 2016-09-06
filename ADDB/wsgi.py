96d6904f07f893727b8b7c520b6accbce2447b12
chingtoe365 <jintao.long@plymouth.ac.uk>
2016-09-06 21:25:01 +0000

    wsgi settings

--- ADDB/wsgi.py
+++ ADDB/wsgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+# add the alzexpress project path into the sys.path
+sys.path.append('/home/alzexpress/alzexpress')
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append('/home/alzexpress/addbenv/lib/python2.7/site-packages')
+
+# pointing to the project settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ADDB.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ADDB.settings")
 
 application = get_wsgi_application()
