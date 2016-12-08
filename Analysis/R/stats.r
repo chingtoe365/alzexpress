3870ea1a2cbcc5d85111d648dcbffc6254924649
chingtoe365 <jintao.long@plymouth.ac.uk>
2016-12-08 22:27:19 +0000

    analysis update for rnaseq data

--- Analysis/R/stats.r
+++ Analysis/R/stats.r
@@ -36,6 +36,7 @@ limma_calculator <- function(df, class_lst) {
     if(any(rownames(df)=="gender")){
         to_drop_ind = c(to_drop_ind, which(rownames(df)=="gender"))   
     }
+    print(to_drop_ind)
     result = list(t_score=as.numeric(fit2i$t)[-to_drop_ind], p_value=as.numeric(fit2i$p.value)[-to_drop_ind])
 
     return(result) 
